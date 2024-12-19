56bd71d26d294299b4f5858727125f9ee3a745bd
yhli <liyahui360@163.com>
2024-12-19 17:42:32 +0800

    first commit

---
+++ Sources/Video.DirectShow/Internals/IAMVideoProcAmp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AForge.Video.DirectShow.Internals
+{
+    [ComImport, System.Security.SuppressUnmanagedCodeSecurity,
+     Guid("C6E13360-30AC-11d0-A18C-00A0C9118956"),
+     InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    interface IAMVideoProcAmp
+    {
+        [PreserveSig]
+        int GetRange(
+            [In] VideoProcAmpProperty Property,
+            [Out] out int pMin,
+            [Out] out int pMax,
+            [Out] out int pSteppingDelta,
+            [Out] out int pDefault,
+            [Out] out VideoProcAmpFlags pCapsFlags
+        );
+
+        [PreserveSig]
+        int Set(
+            [In] VideoProcAmpProperty Property,
+            [In] int lValue,
+            [In] VideoProcAmpFlags Flags
+        );
+
+        [PreserveSig]
+        int Get(
+            [In] VideoProcAmpProperty Property,
+            [Out] out int lValue,
+            [Out] out VideoProcAmpFlags Flags
+        );
+    }
+}
---
+++ Sources/Video.DirectShow/VideoProcAmpProperty.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AForge.Video.DirectShow
+{
+    /// <summary>
+    /// From VideoProcAmpProperty
+    /// </summary>
+    public enum VideoProcAmpProperty
+    {
+        /// <summary>
+        /// 亮度
+        /// </summary>
+        Brightness,
+        /// <summary>
+        /// 对比度
+        /// </summary>
+        Contrast,
+        /// <summary>
+        /// 色相
+        /// </summary>
+        Hue,
+        /// <summary>
+        /// 饱和度
+        /// </summary>
+        Saturation,
+        /// <summary>
+        /// 清晰度
+        /// </summary>
+        Sharpness,
+        /// <summary>
+        /// 伽马
+        /// </summary>
+        Gamma,
+        /// <summary>
+        /// 启用颜色
+        /// </summary>
+        ColorEnable,
+        /// <summary>
+        /// 白平衡
+        /// </summary>
+        WhiteBalance,
+        /// <summary>
+        /// 逆光对比
+        /// </summary>
+        BacklightCompensation,
+        /// <summary>
+        /// 增益
+        /// </summary>
+        Gain
+    }
+
+    /// <summary>
+    /// From VideoProcAmpFlags
+    /// </summary>
+    [Flags]
+    public enum VideoProcAmpFlags
+    {
+        /// <summary>
+        /// 默认
+        /// </summary>
+        None = 0,
+        /// <summary>
+        /// 自动
+        /// </summary>
+        Auto = 0x0001,
+        /// <summary>
+        /// 手动
+        /// </summary>
+        Manual = 0x0002
+    }
+}
